0a9f4fb58a51d7fb0740fd9c264d0e5640d87772
DearSathapat <101991149+DearSathapat@users.noreply.github.com>
2022-11-06 23:56:22 +0700

    Day 65
    Module & Package

---
+++ myprogram.py
@@ -0,0 +1,9 @@
+#def my_func():
+	#print("Hey I am in mymodule.py")
+
+from MyMainPackage import some_main_script
+from MyMainPackage.Subpackage import mysubscript
+
+some_main_script.report_main()
+
+mysubscript.sub_report()
